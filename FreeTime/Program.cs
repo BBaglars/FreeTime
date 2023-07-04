214d2917a2e76d1584b61d662103a117f1c40b1b
LENOVO <LENOVO@LAPTOP-SH11FGSL>
2023-07-04 16:52:44 +0300

    Add project files.

---
+++ FreeTime/Program.cs
@@ -0,0 +1,93 @@
+﻿Console.WriteLine("-Kart ATM'ye Koyulmuştur-");
+Console.WriteLine("\nBu Bir Bankacılık Sistemidir");
+Console.Write("\nLütfen Bakiye Giriniz : ");
+int bakiye = Int32.Parse(Console.ReadLine());
+if (bakiye >= 0)
+{
+
+    Console.WriteLine("\n***İŞLEMLER***");
+    Console.WriteLine("1. Para Çekme");
+    Console.WriteLine("2. Para Yatırma");
+    Console.WriteLine("3. Bakiye Sorgulama ");
+    Console.WriteLine("4. Kart İade");
+
+    Console.Write("\nLütfen İşleminizi Seçiniz : ");
+
+    int islem = Int32.Parse(Console.ReadLine());
+
+    switch (islem)
+    {
+
+        case 1:
+            if (bakiye > 0)
+            {
+                Console.WriteLine("\nMevcut Bakiye = " + bakiye + "\n");
+                Console.Write("Ne Kadar Para Çekilecektir = ");
+                int cekilecekpara = Int32.Parse(Console.ReadLine());
+                if (cekilecekpara > bakiye)
+                {
+                    Console.WriteLine("\nHesapta Yeteri Kadar Para Yoktur");
+                    Console.WriteLine("\nKart İade Ediliyor...");
+                }
+                else if (cekilecekpara <= bakiye == cekilecekpara > 0)
+                {
+                    int yeniBakiye = bakiye - cekilecekpara;
+                    Console.WriteLine("\nPara Çekilmiştir");
+                    Console.WriteLine("\nYeni Bakiye = " + yeniBakiye);
+                    Console.WriteLine("\nKart İade Ediliyor...");
+                }
+                else
+                {
+                    Console.WriteLine("\nBöyle Bir Nakit Bulumamaktadır");
+                    Console.WriteLine("\nKart İade Ediliyor...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nHata Maalesef Çekebilecek Paranız Bulunmamaktadır");
+                Console.WriteLine("\nKart İade Ediliyor...");
+            }
+            break;
+        case 2:
+            Console.WriteLine("\nMevcut Bakiye = " + bakiye + "\n");
+            Console.Write("Lütfen Yatırılacak Tutarı Giriniz = ");
+            int yatirilacakPara = Int32.Parse(Console.ReadLine());
+            if (yatirilacakPara >= 0 == yatirilacakPara <= 50000)
+            {
+                int yeniBakiye = bakiye + yatirilacakPara;
+                Console.WriteLine("\nPara Yatırılmıştır");
+                Console.WriteLine("\nYeni Bakiye = " + yeniBakiye);
+                Console.WriteLine("\nKart İade Ediliyor...");
+            }
+            else if (yatirilacakPara > 50000)
+            {
+                Console.WriteLine("\nHata Maalesef Tek Sferde 50.000TL'den Fazla Nakit Hesapa Yüklenemez");
+                Console.WriteLine("\nKart İade Ediliyor...");
+            }
+            else
+            {
+                Console.WriteLine("\nLütfen Yüklenecek Nakti Pozitif Tam Sayı Olarak Giriniz");
+            }
+
+            break;
+        case 3:
+            Console.WriteLine("\nMevcut Bakiye = " + bakiye);
+            Console.WriteLine("\nKart İade Ediliyor...");
+            break;
+        case 4:
+            Console.WriteLine("\nKart İade Ediliyor...");
+            break; 
+        default:
+            Console.WriteLine("\nBöyle Bir İşlemimiz Bulunmamaktadır");
+            Console.WriteLine("\nKart İade Ediliyor...");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("\nLütfen Bakiyenizi Pozitif Tam Sayı Olarak Giriniz");
+    Console.WriteLine("\nKart İade Ediliyor...");
+}
+
+
+
---
+++ Taksimetre/Program.cs
@@ -0,0 +1,138 @@
+﻿Console.WriteLine("Bu Taksimetre Programıdır \n");
+Console.WriteLine("Taksi Sadece Kredi Kartı İle Ödeme Alır \n");
+double kmBasiUcret = 8.51;
+Console.WriteLine("Yolcu Taksiye Biner...\n");
+Console.Write("Lütfen Alınan Sabit Ücreti Giriniz : ");
+double sabitUcret = Int32.Parse(Console.ReadLine());
+if (sabitUcret < 0)
+{
+    Console.WriteLine("\nLütfen Pozitif Bir Sayı Giriniz");
+    Console.WriteLine("\nYolcu Taksiden İner...");
+}
+else if (sabitUcret >= 30)
+{
+    Console.WriteLine("\nSöylenen Ücret Çok Yüksek. Lütfen Fiyatı Düşürün");
+    Console.WriteLine("\nYolcu Taksiden İner...");
+}
+else
+{
+    Console.WriteLine("\n1 - Kilometre");
+    Console.WriteLine("2 - Metre");
+    Console.Write("\nMesafe Kilometre Üzerinden Mi Yoksa Metre Üzerinden Mi Hesaplanacak? : ");
+    int islem = Int32.Parse(Console.ReadLine());
+    switch (islem)
+    {
+        case 1:
+            Console.Write("\nKaç Kilometre Yol Gidilecek : ");
+            double kmislem = Int32.Parse(Console.ReadLine());
+            if (kmislem <= 0)
+            {
+                Console.WriteLine("\nLütfen Pozitif Bir Sayı Giriniz");
+                Console.WriteLine("\nYolcu Taksiden İner...");
+            }
+            else if (kmislem < 8 && kmislem > 0)
+            {
+                Console.WriteLine("\nKısa Mesafe Yolcu Alımı Olmaz");
+                Console.WriteLine("\nYolcu Taksiden İner...");
+            }
+            else if (kmislem >= 150)
+            {
+                Console.WriteLine("\nLütfen Taksi İçin Uygun Mesafe Giriniz");
+                Console.WriteLine("\nYolcu Taksiden İner...");
+            }
+            else
+            {
+                double toplamUcret = sabitUcret + (kmislem * kmBasiUcret);
+                Console.WriteLine("\nÖdenecek Toplam Tutar = " + toplamUcret + " TL");
+                Console.Write("\nBankada Ne Kadar Para Bulunmakata = ");
+                double bankaPara1 = Int32.Parse(Console.ReadLine());
+                if (bankaPara1 > 0)
+                {
+                    if (bankaPara1 >= toplamUcret)
+                    {
+                        Console.WriteLine("\nKredi Kartı Okutuluyor...");
+                        Console.WriteLine("\nÖdeme Yapılıyor...");
+                        double yeniBankaPara1 = bankaPara1 - toplamUcret;
+                        Console.WriteLine("\nÖdeme Yapıldı.");
+                        Console.WriteLine("\nYeni Bakiye = " + yeniBankaPara1 + "TL");
+                        Console.WriteLine("\nYolcu Taksiden İner...");
+                    }
+                    else if (bankaPara1 < toplamUcret)
+                    {
+                        Console.WriteLine("\nKredi Kartı Okutuluyor...");
+                        Console.WriteLine("\nYeterli Bakiye Bulunmuyor...");
+                        Console.WriteLine("\nÖdeme Yapılamadı...");
+                        Console.WriteLine("\nYolcu Taksiden İner...");
+                    }
+                }
+                else if (bankaPara1 <= 0)
+                {
+                    Console.WriteLine("\nKredi Kartı Okutuluyor...");
+                    Console.WriteLine("\nYeterli Bakiye Bulunmuyor...");
+                    Console.WriteLine("\nÖdeme Yapılamadı...");
+                    Console.WriteLine("\nYolcu Taksiden İner...");
+                }   
+
+            }
+            break;
+        case 2:
+            Console.Write("\nKaç Metre Yol Gidilecek : ");
+            double mislem = Int32.Parse(Console.ReadLine());
+            if (mislem <= 0)
+            {
+                Console.WriteLine("\nLütfen Pozitif Bir Sayı Giriniz");
+                Console.WriteLine("\nYolcu Taksiden İner...");
+            }
+            else if (mislem < 8000 && mislem > 0)
+            {
+                Console.WriteLine("\nKısa Mesafe Yolcu Alımı Olmaz");
+                Console.WriteLine("\nYolcu Taksiden İner...");
+            }
+            else if (mislem >= 150000)
+            {
+                Console.WriteLine("\nLütfen Taksi İçin Uygun Mesafe Giriniz");
+                Console.WriteLine("\nYolcu Taksiden İner...");
+            }
+            else
+            {
+                double mislem2 = mislem/1000;
+                double toplamUcret = sabitUcret + (mislem2 * kmBasiUcret);
+                Console.WriteLine("\nÖdenecek Toplam Tutar = " + toplamUcret + " TL");
+                Console.Write("\nBankada Ne Kadar Para Bulunmakata = ");
+                double bankaPara1 = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("\nKredi Kartı Okutuluyor...");
+                if (bankaPara1 > 0)
+                {
+                    if (bankaPara1 >= toplamUcret)
+                    {
+                        Console.WriteLine("\nKredi Kartı Okutuluyor...");
+                        Console.WriteLine("\nÖdeme Yapılıyor...");
+                        double yeniBankaPara1 = bankaPara1 - toplamUcret;
+                        Console.WriteLine("\nÖdeme Yapıldı.");
+                        Console.WriteLine("\nYeni Bakiye = " + yeniBankaPara1 + "TL");
+                        Console.WriteLine("\nYolcu Taksiden İner...");
+                    }
+                    else if (bankaPara1 < toplamUcret)
+                    {
+                        Console.WriteLine("\nKredi Kartı Okutuluyor...");
+                        Console.WriteLine("\nYeterli Bakiye Bulunmuyor...");
+                        Console.WriteLine("\nÖdeme Yapılamadı...");
+                        Console.WriteLine("\nYolcu Taksiden İner...");
+                    }
+                }
+                else if (bankaPara1 <= 0)
+                {
+                    Console.WriteLine("\nKredi Kartı Okutuluyor...");
+                    Console.WriteLine("\nYeterli Bakiye Bulunmuyor...");
+                    Console.WriteLine("\nÖdeme Yapılamadı...");
+                    Console.WriteLine("\nYolcu Taksiden İner...");
+                }
+            }
+            break;
+        default:
+            Console.WriteLine("\nMalesef Böyle Bir İşlem Yoktur. ");
+            Console.WriteLine("\nYolcu Taksiden İner...");
+            break;
+    }
+}
+
